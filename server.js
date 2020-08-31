aa9bbef72ec9d74e060c18d82ea280c0edaa33c2
mastro <25ayushi1995@gmail.com>
2020-08-31 13:14:10 +0530

    change in script delete file

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
-var spawn = require('child_process').spawn
-// Load the AWS SDK for Node.js
+require('./node_modules/node-cmd/cmd.js')
+var cmd = require('node-cmd')
+const fs = require('fs')
 var AWS = require('aws-sdk')
 // Set the region
 require('dotenv').config()
+console.log(process.env.ACCESS_KEY, 'accesskey')
 AWS.config.update({
   credentials: {
     accessKeyId: process.env.ACCESS_KEY,
@@ -25,30 +27,11 @@ const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
       (err, res) => {
         res.CommonPrefixes.map((item) => {
           let fileName = item.Prefix.split('/')
-          var cmd = '/usr/bin/ffmpeg'
+          var proc = cmd.run(
+            `ffmpeg -i https://vdyoencoded.s3-ap-southeast-1.amazonaws.com/${item.Prefix}play.m3u8 -bsf:a aac_adtstoasc -vcodec copy -c copy ${fileName[2]}.mp4`
+          )
 
-          var args = [
-            '-y',
-            '-i',
-            `https://vdyoencoded.s3-ap-southeast-1.amazonaws.com/${item.Prefix}play.m3u8`,
-            '-s',
-            '1024x576',
-            '-codec:a',
-            'aac',
-            '-b:a',
-            '44.1k',
-            '-r',
-            '15',
-            '-b:v',
-            '1000k',
-            '-c:v',
-            'h264',
-            '-f',
-            'mp4',
-            `${fileName[2]}.mp4`,
-          ]
-
-          var proc = spawn(cmd, args)
+          //var proc = spawn(cmd, args)
 
           proc.stdout.on('data', function (data) {
             console.log(data)
@@ -59,8 +42,35 @@ const s3 = new AWS.S3({ apiVersion: '2006-03-01' })
             console.log(data)
           })
 
-          proc.on('close', function () {
-            console.log('finished')
+          proc.on('close', function (data) {
+            console.log(data, 'finished')
+            const fileContent = fs.readFileSync(`${fileName[2]}.mp4`)
+
+            // Setting up S3 upload parameters
+            const params = {
+              Bucket: 'convertedvydo',
+              Key: `${fileName[2]}.mp4`, // File name you want to save as in S3
+              Body: fileContent,
+            }
+
+            // Uploading files to the bucket
+            s3.upload(params, function (err, data) {
+              if (err) {
+                throw err
+              }
+              let deleteproc = cmd.run(`rm -rf ${fileName[2]}.mp4`)
+              deleteproc.stdout.on('data', function (data) {
+                console.log(data)
+              })
+              deleteproc.stderr.setEncoding('utf8')
+              deleteproc.stderr.on('data', function (data) {
+                console.log(data)
+              })
+              deleteproc.on('close', function (data) {
+                console.log('deleted')
+              })
+              console.log(`File uploaded successfully. ${data.Location}`)
+            })
           })
         })
       }
